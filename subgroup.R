dd5dac6d5e174520ab079357f8fe10a488985eb7
Krittaphas Chaisutyakorn <tiger@Krittaphass-MacBook-Air.local>
2025-08-01 21:59:27 +0700

    add codes for 6 geological region

--- subgroup.R
+++ subgroup.R
@@ -162,11 +162,18 @@ run_analysis_for_subgroup <- function(subgroup_data,
     png(file.path(results_dir, "plot_lag_response.png"))
     plot(pred_stage1, "slices", var = 10, ylab = "RR", main = paste(subgroup_name, "- Single Lag-Response"))
     dev.off()
+    svg(file.path(results_dir, "plot_lag_response.svg"))
+    plot(pred_stage1, "slices", var = 10, ylab = "RR", main = paste(subgroup_name, "- Single Lag-Response"))
+    dev.off()
 
     # b. Plot and save cumulative lag-response curve
     png(file.path(results_dir, "plot_cumulative_lag_response.png"))
     plot(pred_stage1, "slices", var = 10, cumul = TRUE, ylab = "Cumulative RR", main = paste(subgroup_name, "- Single Cumulative Lag-Response"))
     dev.off()
+    svg(file.path(results_dir, "plot_cumulative_lag_response.svg"))
+    plot(pred_stage1, "slices", var = 10, cumul = TRUE, ylab = "Cumulative RR", main = paste(subgroup_name, "- Single Cumulative Lag-Response"))
+    dev.off()
+
 
     # c. Save single RR data (lagged and cumulative)
 
@@ -174,7 +181,7 @@ run_analysis_for_subgroup <- function(subgroup_data,
     colnames(RR_pooled_lagged) <- c("RR", "Lower", "Upper")
 
     RR_pooled_cumulative <- with(pred_stage1, t(rbind(cumRRfit, cumRRlow, cumRRhigh)))
-    colnames(RR_pooled_cumulative) <- c("CumRR", "Lower", "Upper")
+    colnames(RR_pooled_cumulative) <- c("RR", "Lower", "Upper")
 
     RR_pooled_lagged <- as.data.frame(RR_pooled_lagged)
     RR_pooled_cumulative <- as.data.frame(RR_pooled_cumulative)
@@ -184,7 +191,7 @@ run_analysis_for_subgroup <- function(subgroup_data,
 
     # properly sort the columns order
     RR_pooled_lagged <- RR_pooled_lagged[, c("Lag", "RR", "Lower", "Upper")]
-    RR_pooled_cumulative <- RR_pooled_cumulative[, c("Lag", "CumRR", "Lower", "Upper")]
+    RR_pooled_cumulative <- RR_pooled_cumulative[, c("Lag", "RR", "Lower", "Upper")]
 
     write.csv(RR_pooled_lagged, file.path(results_dir, "pooled_lagged_RR.csv"), row.names = FALSE)
     write.csv(RR_pooled_cumulative, file.path(results_dir, "pooled_cumulative_RR.csv"), row.names = FALSE)
@@ -194,7 +201,9 @@ run_analysis_for_subgroup <- function(subgroup_data,
 
     cat("\nAnalysis for subgroup", subgroup_name, "is complete.\n")
 
-    return() # Exit the function if not enough data to pool
+    RR_pooled <- rbind(RR_pooled_lagged, RR_pooled_cumulative)
+
+    return(RR_pooled) # Exit the function if not enough data to pool
   }
 
   # Run meta-analysis
@@ -228,9 +237,15 @@ run_analysis_for_subgroup <- function(subgroup_data,
   png(file.path(results_dir, "plot_lag_response.png"))
   plot(cpred_pooled, "slices", var = 10, ylab = "RR", main = paste(subgroup_name, "- Pooled Lag-Response"))
   dev.off()
+  svg(file.path(results_dir, "plot_lag_response.svg"))
+  plot(cpred_pooled, "slices", var = 10, ylab = "RR", main = paste(subgroup_name, "- Pooled Lag-Response"))
+  dev.off()
 
   # c. Plot and save cumulative lag-response curve
   png(file.path(results_dir, "plot_cumulative_lag_response.png"))
+  plot(cpred_pooled, "slices", var = 10, cumul = TRUE, ylab = "Cumulative RR", main = paste(subgroup_name, "- Pooled Cumulative Lag-Response"))
+  dev.off()
+  svg(file.path(results_dir, "plot_cumulative_lag_response.svg"))
   plot(cpred_pooled, "slices", var = 10, cumul = TRUE, ylab = "Cumulative RR", main = paste(subgroup_name, "- Pooled Cumulative Lag-Response"))
   dev.off()
 
@@ -239,7 +254,7 @@ run_analysis_for_subgroup <- function(subgroup_data,
   colnames(RR_pooled_lagged) <- c("RR", "Lower", "Upper")
 
   RR_pooled_cumulative <- with(cpred_pooled, t(rbind(cumRRfit, cumRRlow, cumRRhigh)))
-  colnames(RR_pooled_cumulative) <- c("CumRR", "Lower", "Upper")
+  colnames(RR_pooled_cumulative) <- c("RR", "Lower", "Upper")
 
   RR_pooled_lagged <- as.data.frame(RR_pooled_lagged)
   RR_pooled_cumulative <- as.data.frame(RR_pooled_cumulative)
@@ -249,7 +264,7 @@ run_analysis_for_subgroup <- function(subgroup_data,
 
   # properly sort the columns order
   RR_pooled_lagged <- RR_pooled_lagged[, c("Lag", "RR", "Lower", "Upper")]
-  RR_pooled_cumulative <- RR_pooled_cumulative[, c("Lag", "CumRR", "Lower", "Upper")]
+  RR_pooled_cumulative <- RR_pooled_cumulative[, c("Lag", "RR", "Lower", "Upper")]
 
   write.csv(RR_pooled_lagged, file.path(results_dir, "pooled_lagged_RR.csv"), row.names = FALSE)
   write.csv(RR_pooled_cumulative, file.path(results_dir, "pooled_cumulative_RR.csv"), row.names = FALSE)
@@ -258,6 +273,10 @@ run_analysis_for_subgroup <- function(subgroup_data,
   save(ymat_pollutant, list_vcov_pollutant, file = file.path(results_dir, "first_stage_estimates.RData"))
 
   cat("\nAnalysis for subgroup", subgroup_name, "is complete.\n")
+
+  RR_pooled <- rbind(RR_pooled_lagged, RR_pooled_cumulative)
+
+  return(RR_pooled)
 }
 
 
@@ -271,6 +290,25 @@ cat("Loading and preparing the main dataset...\n")
 # Make sure the working directory is set to your project root
 # setwd("~/Projects/hf_pm_analysis")
 dat_full <- read.csv("data/daily_all_weather_30jul.csv")
+
+region_list <- list(
+  North = c("Chiang Rai", "Nan", "Phayao", "Chiang Mai", "Mae Hong Son", "Phrae", "Lampang", "Lamphun", "Uttaradit"),
+  Northeast = c("Nong Khai", "Nakhon Phanom", "Sakon Nakhon", "Udon Thani", "Loei", "Mukdahan", "Khon Kaen", "Nakhon Ratchasima", "Buri Ram", "Ubon Ratchathani"),
+  West = c("Tak", "Kanchanaburi", "Ratchaburi", "Prachuap Khiri Khan"),
+  Central = c("Bangkok", "Phitsanulok", "Sukhothai", "Phetchabun", "Phichit", "Kamphaeng Phet", "Nakhon Sawan", "Lop Buri", "Uthai Thani", "Saraburi", "Phra Nakhon Si Ayutthaya", "Suphan Buri", "Pathum Thani", "Nonthaburi", "Nakhon Pathom", "Samut Prakan", "Samut Sakhon", "Samut Songkhram"),
+  East = c("Sa Kaeo", "Prachin Buri", "Chachoengsao", "Chon Buri", "Rayong", "Chanthaburi", "Trat"),
+  South = c("Nakhon Si Thammarat", "Phuket", "Surat Thani", "Trang", "Pattani", "Songkhla", "Satun", "Narathiwat", "Yala")
+)
+
+# 2. Convert the list into a two-column dataframe for mapping
+# This creates a lookup table with 'prov_name' and 'region'
+region_map_df <- stack(region_list) %>%
+  rename(prov_name = values, region = ind)
+
+# 3. Join the region mapping to your main dataframe 'dat'
+# This assumes your dataframe is named 'dat' and has a 'prov_name' column
+dat_full <- dat_full %>%
+  left_join(region_map_df, by = "prov_name")
 
 # Properly format date and create time-stratification variables
 dat_full$date_start <- as.Date(dat_full$date_start, "%Y-%m-%d")
@@ -304,6 +342,9 @@ subgroups <- subgroups[!is.na(subgroups)] # Remove NA from the list of subgroups
 
 cat(paste("\nFound", length(subgroups), "subgroups to analyze from column '", SUBGROUPING_COLUMN, "'.\n", sep = " "))
 
+# initiate list to store result from subgroup analysis
+RR_pooled_list <- list()
+
 # Loop through each subgroup, filter data, and run the analysis function
 for (group in subgroups) {
   # Create a clean name for the directory (e.g., "region_1")
@@ -314,7 +355,7 @@ for (group in subgroups) {
     filter(.data[[SUBGROUPING_COLUMN]] == group)
 
   # Call the main analysis function
-  run_analysis_for_subgroup(
+  RR_pooled_subgroup <- run_analysis_for_subgroup(
     subgroup_data = data_for_group,
     subgroup_name = subgroup_folder_name,
     pollutant_col_name = POLLUTANT_VAR,
@@ -322,13 +363,16 @@ for (group in subgroups) {
     lag_number = LAG,
     base_output_dir = BASE_OUTPUT_DIR
   )
+
+  # add to list of results
+  RR_pooled_list[[group]] <- RR_pooled_subgroup
 }
 
 cat("\n\nAll subgroup analyses are complete.\n")
 
 cat("\nPrinting NHSO region codes mapping.")
 
-nhso_code_map <- dat %>%
+nhso_code_map <- dat_full %>%
   select(nhso_region_code, prov_name) %>%
   unique() %>%
   group_by(nhso_region_code) %>%
@@ -336,5 +380,154 @@ nhso_code_map <- dat %>%
   unique() %>%
   arrange(nhso_region_code)
 write.csv(nhso_code_map, file.path(BASE_OUTPUT_DIR, "nhso_region_codes_mapping.csv"), row.names = FALSE)
+
+geo_code_map <- dat_full %>%
+  select(region, prov_name) %>%
+  unique() %>%
+  group_by(region) %>%
+  mutate(prov_name = paste(prov_name, collapse = ", ")) %>%
+  unique() %>%
+  arrange(region)
+write.csv(geo_code_map, file.path(BASE_OUTPUT_DIR, "regions_mapping.csv"), row.names = FALSE)
+
+### plot forest plot pooled estimate for each subgroup
+
+# read pooled estimates for all provinces
+RR_pooled_lagged <- read.csv("/Users/tiger/Projects/hf_pm_analysis/output/30jul/results_PM2.5-hf_prim-7lag/pooled_lagged_RR.csv") %>%
+  filter(Lag == 0) %>%
+  mutate(
+    RR = round(RR, 4),
+    Lower = round(Lower, 4),
+    Upper = round(Upper, 4),
+    Region = "Pooled",
+    pooled = TRUE
+  ) %>%
+  select(-Lag) %>%
+  select(c("Region", "RR", "Lower", "Upper", "pooled"))
+
+for (i in seq(subgroups)) {
+  subgroup_rr <- RR_pooled_list[[i]]["lag0", ] %>%
+    mutate(
+      RR = round(RR, 4),
+      Lower = round(Lower, 4),
+      Upper = round(Upper, 4),
+      Region = subgroups[i],
+      pooled = FALSE
+    ) %>%
+    select(c("Region", "RR", "Lower", "Upper", "pooled"))
+  RR_pooled_lagged <- rbind(RR_pooled_lagged, subgroup_rr)
+}
+
+if (SUBGROUPING_COLUMN == "region") {
+  # If the subgroup == region, This if loop is for properly sorting of the region order
+  forest_subgroup_plot <- RR_pooled_lagged %>%
+    mutate(across(c(RR, Upper, Lower), as.numeric)) %>%
+    mutate(sort_order = ifelse(pooled, 0, as.numeric(factor(Region)))) %>%
+    arrange(desc(sort_order), Region) %>%
+    mutate(Region = factor(Region, levels = unique(Region))) %>%
+    ggplot(aes(
+      x = RR,
+      y = Region,
+      color = pooled
+    )) +
+    geom_errorbarh(
+      aes(
+        xmin = Lower,
+        xmax = Upper,
+      )
+    ) +
+    geom_point(data = . %>% filter(pooled == FALSE), size = 2, shape = 16) +
+    geom_point(data = . %>% filter(pooled == TRUE), size = 3, shape = 18) +
+    geom_vline(xintercept = 1, linetype = "dashed", color = "black", linewidth = 0.5, alpha = 0.6) +
+    scale_x_continuous(breaks = scales::breaks_pretty(n = 5)) +
+    labs(
+      title = paste(
+        "Relative Risk per 10 μg/m³ increase of",
+        pollutant_col_name,
+        "\n By Region",
+        sep = " "
+      ),
+      x = "Relative Risk (RR) with 95% Confidence Interval",
+      y = "Region"
+    ) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(hjust = 0.5, face = "bold"),
+      legend.position = "none"
+    )
+  ggsave(
+    file.path(BASE_OUTPUT_DIR, paste0("forest_plot_", SUBGROUPING_COLUMN, "_", pollutant_col_name, ".png")),
+    plot = forest_subgroup_plot,
+    width = 8,
+    height = 12,
+    units = "in",
+    dpi = 300,
+    bg = "white"
+  )
+  ggsave(
+    file.path(BASE_OUTPUT_DIR, paste0("forest_plot_", SUBGROUPING_COLUMN, "_", pollutant_col_name, ".svg")),
+    plot = forest_subgroup_plot,
+    width = 8,
+    height = 12,
+    units = "in",
+    dpi = 300,
+    bg = "white"
+  )
+} else {
+  # If the subgroup == nhso_region_code, This if loop is for properly sorting of the region order
+  forest_subgroup_plot <- RR_pooled_lagged %>%
+    mutate(across(c(RR, Upper, Lower), as.numeric)) %>%
+    mutate(sort_order = ifelse(pooled, 0, as.numeric(Region))) %>%
+    arrange(desc(sort_order)) %>%
+    mutate(Region = factor(Region, levels = unique(Region))) %>%
+    ggplot(aes(
+      x = RR,
+      y = Region,
+      color = pooled
+    )) +
+    geom_errorbarh(
+      aes(
+        xmin = Lower,
+        xmax = Upper,
+      )
+    ) +
+    geom_point(data = . %>% filter(pooled == FALSE), size = 2, shape = 16) +
+    geom_point(data = . %>% filter(pooled == TRUE), size = 3, shape = 18) +
+    geom_vline(xintercept = 1, linetype = "dashed", color = "black", linewidth = 0.5, alpha = 0.6) +
+    scale_x_continuous(breaks = scales::breaks_pretty(n = 5)) +
+    labs(
+      title = paste(
+        "Relative Risk per 10 μg/m³ increase of",
+        pollutant_col_name,
+        "\n By NHSO Region Code",
+        sep = " "
+      ),
+      x = "Relative Risk (RR) with 95% Confidence Interval",
+      y = "NHSO Region Code"
+    ) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(hjust = 0.5, face = "bold"),
+      legend.position = "none"
+    )
+  ggsave(
+    file.path(BASE_OUTPUT_DIR, paste0("forest_plot_", SUBGROUPING_COLUMN, "_", pollutant_col_name, ".png")),
+    plot = forest_subgroup_plot,
+    width = 8,
+    height = 12,
+    units = "in",
+    dpi = 300,
+    bg = "white"
+  )
+  ggsave(
+    file.path(BASE_OUTPUT_DIR, paste0("forest_plot_", SUBGROUPING_COLUMN, "_", pollutant_col_name, ".svg")),
+    plot = forest_subgroup_plot,
+    width = 8,
+    height = 12,
+    units = "in",
+    dpi = 300,
+    bg = "white"
+  )
+}
 
 cat("=====Finished=====")
